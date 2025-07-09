98e8c31869b5b4b4d7fe78e2b6b4efb805e1f10d
Kirina Rolando <kirinarolando@gmail.com>
2025-07-09 20:15:52 -0300

    adding sidebar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,28 @@
 import logo from '../../assets/img/logo_biblioteca_rivadavia.svg';
-import menuleft from '../../assets/img/menu_arrow_left.svg';
+import btnopen from '../../assets/img/menu_arrow_open.svg';
 import './Header.css';
-
+import { useState } from 'react';
+import Sidebar from '../sidebar/Sidebar.jsx';
 export default function Header(){
+    const [open,setOpen]= useState(false);
+
+    const toggleMenu =()  =>{
+        setOpen(!open);
+    }
+
     return (
         <>
     <header className="header">
             <div className="header-container">
-                <div className="menu">
-                    <img src={menuleft} alt="Menú" />
-                </div>
+             <button onClick={toggleMenu} className="menu-button">
+                    <div className="btnmenuimg">
+                        <img
+                        src={btnopen}
+                        alt="Menú"
+                        className={open ? 'rotate' : ''}
+                        />
+                    </div>
+              </button>
                 <div className="title">
                     <div> 
                         <div className='logo' > 
@@ -17,10 +30,17 @@ export default function Header(){
                         </div>
                           <h1>Biblioteca Popular Bernardino Rivadavia</h1>
                     </div>
-                   
                 </div>
             </div>
     </header>
+
+    
+
+               
+                <Sidebar isOpen={open} />
+            
         </>
     )
 }
+
+    
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import './Sidebar.css';
+import loan from '../../assets/img/loan-icon.svg';
+import book from '../../assets/img/book-icon.svg';
+import fee from '../../assets/img/fee-icon.svg';
+import author from '../../assets/img/author-icon.svg'
+import options from '../../assets/img/options-icon.svg';
+import partner from '../../assets/img/options-icon.svg';
+export default function Sidebar({ isOpen }) {
+
+    return   (
+        <>
+          <nav className={`sidebar ${isOpen ? 'sidebar-active' : ''}`}>
+            <h3><strong>Javier Ondicol</strong></h3>
+            <h3>Administrador</h3>
+                    <ul className="listsidebar">
+                        <li><div className='iconssidebar'><img src={book} alt="Libros" /></div> <a href="">Libros</a></li>
+                        <li><div className='iconssidebar' ><img src={author} alt="Autor" /></div> <a href="">Autor</a></li>
+                        <li><div className='iconssidebar'><img src={partner} alt="Socio" /></div> <a href="">Socios </a></li>
+                        <li><div className='iconssidebar'><img src={fee} alt="Socio" /></div> <a href="">Cuotas</a></li>
+                        <li><div className='iconssidebar'><img src={options} alt="Opciones" /></div> <a href="">Opciones</a></li>
+                    </ul>
+                </nav> 
+        </>
+    )
+}
