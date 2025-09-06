1d056f1b48f824b02204d1622ee5639a71af562f
kirina <kirinarolando@gmail.com>
2025-09-06 18:33:19 -0300

    fixed header

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -15,32 +15,39 @@ export default function Header() {
   return (
     <>
       <header className="header">
-        <div className="header-container">
-          <div className="menu-wrapper">
-            <button onClick={toggleMenu} className="menu-button">
-              <div className="btnmenuimg">
-                <img src={btnopen} alt="Menú" className={open ? 'rotate' : ''} />
-              </div>
-            </button>
-          </div>
+          <div className="header-container">
+            
+            <div className="header-left">
+              <button onClick={toggleMenu} className="menu-button">
+                <div className="btnmenuimg">
+                  <img src={btnopen} alt="Menú" className={open ? 'rotate' : ''} />
+                </div>
+              </button>
+            </div>
 
-          <div className="title-wrapper">
-            <a href="/" className="logo">
-              <img src={logo} alt="Logo Biblioteca Rivadavia" />
-            </a>
-            <h1 className="title-text">Biblioteca Popular Bernardino Rivadavia</h1>
+            <div className="header-center">
+              <a href="/" className="logo">
+                <img src={logo} alt="Logo Biblioteca Rivadavia" />
+              </a>
+              <h1 className="title-text">Biblioteca Popular Bernardino Rivadavia</h1>
+            </div>
+
+            <div className="header-right">
+              {!authMock.isAuthenticated ? (
+                <div className="auth-buttons">
+                  <Btn text="Ingresar" variant="login" href="/login" />
+                  <Btn text="Registrarse" variant="register" href="/register" />
+                </div>
+              ) : (
+                <h3 className="header-msg">
+                  Bienvenido <span className="header-name">{authMock.name}</span>
+                </h3>
+              )}
+            </div>
+
           </div>
-       {!authMock.isAuthenticated ? (
-          <div className="auth-buttons">
-            <Btn text="Ingresar" variant="login" href="/login" />
-            <Btn text="Registrarse" variant="register" href="/register" />
-          </div>
-        ) : 
-        <h3 className='header-msg'>Bienvenido <span className='header-name'>{authMock.name}</span></h3>
-        }
-        </div>
-      </header>
-      <Sidebar isOpen={open} />
+          <Sidebar isOpen={open} />
+        </header>
     </>
   );
 }
