0b52a5a3e8185480be9c6a852f494d37f28f9297
javier <javierse9@gmail.com>
2025-09-30 12:43:31 -0300

    adding more relationships between models

--- backend/src/models/relationships.js
+++ backend/src/models/relationships.js
@@ -15,27 +15,45 @@ import BookTypeGroup from '../models/options/BookTypeGroup.js';
 import BookTypeGroupList from '../models/options/BookTypeGroupList.js';
 import Employees from '../models/options/Employees.js';
 import BookKey from './book/BookKey.js';
+import BookType from './book/BookType.js';
 
 //authors
+//Autor tiene muchos LibroAutor
 Author.hasMany(BookAuthor, { foreignKey: 'authorCode' });
+
+//LibroAutor pertenece a Autor y a Libro
 BookAuthor.belongsTo(Author, { foreignKey: 'authorCode' });
+BookAuthor.belongsTo(Book, { foreignKey: 'idBook' });
 
 //loans
+//Prestamo tiene muchos TipoPrestamo
 Loan.hasMany(LoanType, { foreignKey: 'id' });
+
+//TipoPrestamo pertenece a Prestamo
 LoanType.belongsTo(Loan, { foreignKey: 'id' });
 
+//Prestamo tiene muchos empleados
 Loan.hasMany(Employees, { foreignKey: 'id' });
+
+//Empleado pertenece a Prestamo
 Employees.belongsTo(Loan, { foreignKey: 'id' });
 
+//Reservas tiene muchos ReservaLibro
 Reservations.hasMany(BookReservations, { foreignKey: 'reservationId'});
 
+//Prestamo tiene muchos PrestamoLibro
 Loan.hasMany(LoanBook, { foreignKey: 'loanId' });
+
+//PrestamoLibro pertenece a Prestamo y Libro
 LoanBook.belongsTo(Loan, { foreignKey: 'loanId' });
+LoanBook.belongsTo(Book, { foreignKey: 'idBook' });
 
 //BookTypeGroup se relaciona con BookType 
 BookTypeGroupList.hasMany(BookTypeGroup, { foreignKey: 'groupId' });
-BookTypeGroup.belongsTo(BookTypeGroupList, { foreignKey: 'groupId' });
 
+//TipoGrupoLibro pertenece a GrupoTipoLibro y TipoLibro
+BookTypeGroup.belongsTo(BookTypeGroupList, { foreignKey: 'groupId' });
+BookTypeGroup.belongsTo(BookType, { foreignKey: 'id' });
 
 // ReservaLibro pertenece a Libro y Reserva
 BookReservations.belongsTo(Book, { foreignKey: 'idBook' });
@@ -46,7 +64,6 @@ Book.hasMany(BookReservations, { foreignKey: 'idBook' }); //se pone la FK en boo
 
 // Reserva tiene muchas ReservaLibro
 Reservations.hasMany(BookReservations, { foreignKey: 'reservationId' });
-
 
 BookKey.belongsTo(Key, { foreignKey: 'id' });
 BookKey.belongsTo(Book, { foreignKey: 'idBook' });
