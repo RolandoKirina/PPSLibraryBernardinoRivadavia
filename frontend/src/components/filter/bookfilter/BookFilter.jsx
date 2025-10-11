fb44b1646819a39e11a12d143ecf55042013f951
kirina <kirinarolando@gmail.com>
2025-10-11 19:01:13 -0300

    adding communication between frontend and backend

--- frontend/src/components/filter/bookfilter/BookFilter.jsx
+++ frontend/src/components/filter/bookfilter/BookFilter.jsx
@@ -1,57 +1,53 @@
 import './BookFilter.css';
+import { useState } from 'react';
+export default function BookFilter({ formData, onChange })  {
 
-export default function BookFilter() {
-
-  return (
+ 
+   return (
     <aside className="book-filter-aside">
-
-
       <div className="book-filter-form">
-
         <form>
           <h3 className='titleh3'>Filtro de libros</h3>
+
           <div className="book-form-input-group">
-            <label>Autor </label>
-            <input type="text" />
+            <label>Autor</label>
+            <input type="text" name="author" value={formData.author} onChange={onChange} />
           </div>
 
           <div className="book-form-input-group">
-            <label>Código de inventario </label>
-            <input type="text" />
+            <label>Código de inventario</label>
+            <input type="text" name="codeInventory" value={formData.codeInventory} onChange={onChange} />
           </div>
 
           <div className="book-form-input-group cdu">
-            <label className='labelinput'>Código de CDU </label>
-
+            <label className='labelinput'>Código de CDU</label>
             <div className='content'>
               <div className='inputCDU'>
-                <input type="text" />
+                <input type="text" name="codeCDU" value={formData.codeCDU} onChange={onChange} />
               </div>
-              <input type="text" className='inputCodeCDU' />
+              <input type="text" className='inputCodeCDU' disabled />
             </div>
           </div>
 
           <div className="book-form-input-group">
-            <label>Codigo de signatura </label>
-            <input type="text" />
+            <label>Código de signatura</label>
+            <input type="text" name="codeSignature" value={formData.codeSignature} onChange={onChange} />
           </div>
 
           <h3 className='titleh3'>Edición</h3>
 
           <div className="book-form-input-group">
-            <label>Año </label>
-            <input type="date" />
+            <label>Año</label>
+            <input type="date" name="yearEdition" value={formData.yearEdition} onChange={onChange} />
           </div>
-
 
           <div className="book-form-input-group">
-            <label>Numero </label>
-            <input type="number" />
+            <label>Número</label>
+            <input type="number" name="numberEdition" value={formData.numberEdition} onChange={onChange} />
           </div>
-
-
         </form>
       </div>
     </aside>
   );
 }
+
