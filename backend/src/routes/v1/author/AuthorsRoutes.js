951d5baed06735e7ad47c0efb8adb0da3f832e91
javier <javierse9@gmail.com>
2026-03-26 16:20:23 -0300

    authors, readers, and options sections api documented

--- backend/src/routes/v1/author/AuthorsRoutes.js
+++ backend/src/routes/v1/author/AuthorsRoutes.js
@@ -5,20 +5,12 @@ import * as AuthorsController from '../../../controllers/author/AuthorsControlle
 
 import { authorizeRoles } from "../../../middlewares/authorizeRoles.js";
 
-
 const router = express.Router();
 
 router.get('/', AuthorsController.getAllAuthors);
 router.get('/:id', authorizeRoles(['admin']), validateIdParam("id"), AuthorsController.getAuthor);
 router.post('/', authorizeRoles(['admin']), AuthorsController.createAuthor);
 router.put('/:id', authorizeRoles(['admin']), validateIdParam("id"), AuthorsController.updateAuthor);
-
-// quizás añadir patch
-// router.patch('/:id', AuthorsController.patchAuthor);
-
 router.delete('/:id', authorizeRoles(['admin']), validateIdParam("id"), AuthorsController.removeAuthor);
-
-//author by name
-
 
 export default router;
